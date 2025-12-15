e8282648bb716938980c782f5af33e785c3fade3
viruskizz <kizzaraiva@gmail.com>
2025-12-15 09:31:43 +0700

    update

---
+++ src/q01/grade.ts
@@ -0,0 +1,16 @@
+const input = ProcessingInstruction.arguments[2];
+const point = Number(input);
+
+if (point < 0 || point > 100){
+  console.log("invalid number")
+} else if (point >= 80) {
+  console.log("A")
+} else if (point >= 70) {
+  console.log("B")
+} else if (point >= 60) {
+  console.log("C")
+} else if (point >= 50) {
+  console.log("D")
+} else {
+  console.log("F")
+}
